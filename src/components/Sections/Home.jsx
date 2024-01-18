6a9c5e126651511e052eb06c3db5114220682aec
Ayla10-kodehode <ayla10.kodehode@gmail.com>
2024-01-18 09:55:53 +0100

    HiddenSection texgt

--- src/components/Sections/Home.jsx
+++ src/components/Sections/Home.jsx
@@ -4,9 +4,13 @@ export const Home = () => {
 
     return(
         <section id="HomeSec">
-            
-            <h1>Front-end <br/>Utvikler</h1>
-            <div>Img</div>
+            <h1 className="Hidden">Home Section</h1>
+            <div className="HeroSec">
+            <div className="TextBox">
+                <h1>Front-end <br/>Utvikler</h1>
+            </div>
+            <img src='/public/Pop-out.svg'></img>
+            </div>
         </section>
     )
 }
